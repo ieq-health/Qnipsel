b32c31051cfa726f89ab207d6bbc45c69399ab12
elephantastic <elephantastic@trashbukk.it>
2018-09-29 14:46:50 +0200

    Add codeview

--- content.php
+++ content.php
@@ -1,1 +1,60 @@
-<?php the_content();
+<?php
+
+$sections = carbon_get_the_post_meta('crb_sections');
+foreach ($sections as $section) {
+    switch ($section['_type']) {
+        case 'text':
+            ?>
+            <div class="section-text">
+                <?= wpautop($section['text']) ?>
+            </div>
+            <?php
+            break;
+        
+        case 'codepen':
+            $title = $section['title'];
+            $html = $section['html'];
+            $css = $section['css'];
+            $js = $section['js'];
+            ?>
+            <div class="card-group codeview">
+                <div class="card codeview__preview-card">
+                    <div class="card-body">
+                        <iframe id="<?= $title ?>" class="codeview__preview-frame"></iframe>
+                    </div>
+                </div>
+                <div class="card codeview__code">
+                    <div class="card-header">
+                        <ul class="nav nav-pills card-header-pills" id="<?= $title ?>Tab" role="tablist">
+                            <li class="nav-item"><a class="nav-link active" data-toggle="tab" role="tab" id="<?= $title ?>-tab-html" href="#<?= $title ?>-html">HTML</a></li>
+                            <li class="nav-item"><a class="nav-link" data-toggle="tab" role="tab" id="<?= $title ?>-tab-css"  href="#<?= $title ?>-css" >CSS</a></li>
+                            <li class="nav-item"><a class="nav-link" data-toggle="tab" role="tab" id="<?= $title ?>-tab-js"   href="#<?= $title ?>-js"  >JS</a></li>
+                        </ul>
+                    </div>
+                    <div class="card-body">
+                        <div class="tab-content" id="<?= $title ?>TabContent">
+                            <div class="tab-pane fade active show" role="tabpanel" id="<?= $title ?>-html">
+                                <code><pre><?= $html ?></pre></code>
+                            </div>
+                            <div class="tab-pane fade" role="tabpanel" id="<?= $title ?>-css">
+                                <code><pre><?= $css ?></pre></code>
+                            </div>
+                            <div class="tab-pane fade" role="tabpanel" id="<?= $title ?>-js">
+                                <code><pre><?= $js ?></pre></code>
+                            </div>                   
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <script>
+                let iframe = document.getElementById('<?= $section['title'] ?>').contentWindow.document;
+                iframe.open();
+                iframe.write(`<style><?= $css ?></style>`);
+                iframe.write(`<body><?= $html ?></body>`);
+                iframe.write(`<script><?= $js ?><\/script>`);
+                iframe.close();
+            </script>
+            <?php
+    }
+}
--- functions.php
+++ functions.php
@@ -1,9 +1,40 @@
 <?php
 
+use Carbon_Fields\Container;
+use Carbon_Fields\Field;
+
+function templateq_crb_attach_post_options()
+{
+    Container::make('post_meta', 'Sections')
+        ->where('post_type', '=', 'page')
+        ->add_fields(array(
+            Field::make('complex', 'crb_sections', 'Sections')
+                ->add_fields('text', 'Text', array(
+                    Field::make('rich_text', 'text', 'Text')
+                ))
+
+                ->add_fields('codepen', 'Codepen', array(
+                    Field::make('text', 'title', 'Titel'),
+                    Field::make('textarea', 'html', 'HTML'),
+                    Field::make('textarea', 'css', 'CSS'),
+                    Field::make('textarea', 'js', 'JS'),
+                ))
+        ));
+}
+add_action('carbon_fields_register_fields', 'templateq_crb_attach_post_options');
+
+function templateq_crb_load()
+{
+    require_once('vendor/autoload.php');
+    \Carbon_Fields\Carbon_Fields::boot();
+}
+add_action('after_setup_theme', 'templateq_crb_load');
+
 function templateq_enqueue()
 {
     // Pull Bootstrap
     wp_enqueue_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css', array(), '4.1.3');
+    wp_enqueue_script('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js', array('jquery'), '4.1.3');
 
     // Pull latest jQuery
     wp_deregister_script('jquery');
@@ -14,6 +45,7 @@ function templateq_enqueue()
     wp_enqueue_script('mmenu', 'https://cdnjs.cloudflare.com/ajax/libs/jQuery.mmenu/7.0.6/jquery.mmenu.all.js', array('jquery'), '7.0.6');
 
     // Pull custom CSS
+    wp_enqueue_style('templateq_codeview', get_template_directory_uri() . '/css/codeview.css', array('bootstrap'), '0.0.1');
     wp_enqueue_style('templateq_dashboard', get_template_directory_uri() . '/css/dashboard.css', array('bootstrap'), '0.0.1');
 
     // Pull custom JS
