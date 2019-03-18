8f4958a5a5d3dfc8c2ef7385cc42c09ca5996c39
Christian Brassat <elephantastic@trashbukk.it>
2019-03-18 11:50:39 +0000

    Update 'page-xml-stripper.php'

--- page-xml-stripper.php
+++ page-xml-stripper.php
@@ -1,26 +1,48 @@
-<div class="container">
-	<div class="content">
-		<h1 class="title"><?php the_title(); ?></h1>
+<?php get_header(); ?>
+
+<aside class="menu">
+	<!-- Logo -->
+	<div class="logo">
+		<img src="/wp-content/themes/templateq/img/logo.svg" alt="Qnipsel">
 	</div>
-</div>
 
-<div class="container-fluid">
-	<div class="columns">
-		<div class="column"><textarea class="textarea" name="" id="input" cols="30" rows="10"></textarea></div>
-		<div class="column"><textarea class="textarea" name="" id="output" cols="30" rows="10"></textarea></div>
-	</div>
-</div>
+	<nav>
+		<ul class="menu-list">
+			<?php wp_list_pages(array('title_li' => null)); ?>
+		</ul>
+	</nav>
+</aside>
 
-	<script>
-		$(function() {
-			$('#input').on('input propertychange', function() {
-				let xml = $(this).val();
+<main>
+	<?php if (have_posts()): ?>
+		<?php while (have_posts()): the_post(); ?>
+			<div class="container">
+				<div class="content">
+					<h1 class="title"><?php the_title(); ?></h1>
+				</div>
+			</div>
 
-				xml = xml.replace(/.*<lastmod.*\n/g, '');
-				xml = xml.replace(/.*<priority.*\n/g, '');
+			<div class="container-fluid">
+				<div class="columns">
+					<div class="column"><textarea class="textarea" name="" id="input" cols="30" rows="10"></textarea></div>
+					<div class="column"><textarea class="textarea" name="" id="output" cols="30" rows="10"></textarea></div>
+				</div>
+			</div>
+		<?php endwhile; ?>
+	<?php endif; ?>
+</main>
 
-				$('#output').val(xml);
-			});
+
+
+<script>
+	$(function() {
+		$('#input').on('input propertychange', function() {
+			let xml = $(this).val();
+
+			xml = xml.replace(/.*<lastmod.*\n/g, '');
+			xml = xml.replace(/.*<priority.*\n/g, '');
+
+			$('#output').val(xml);
 		});
-	</script>
-</div>
+	});
+</script>
