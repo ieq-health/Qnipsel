7736c35d9a7203a7ec0106a8a80a2753c225a5a7
Christian Brassat <elephantastic@trashbukk.it>
2019-03-18 15:11:26 +0000

    Add 'js/tabview.js'

---
+++ js/tabview.js
@@ -0,0 +1,18 @@
+$(function() {
+	$('.tabview').each(function() {
+		let $tabs = $(this).find('.tabs a');
+		let $tabcontents = $(this).find('.tab-content > div');
+
+		$tabs.on('click', function(e) {
+			let $target = $($(this).attr('href'));
+
+			$tabs.parent('li').removeClass('is-active');
+			$tabcontents.removeClass('is-active');
+
+			$(this).parent('li').addClass('is-active');
+			$target.addClass('is-active');
+
+			e.preventDefault();
+		});
+	});
+});
