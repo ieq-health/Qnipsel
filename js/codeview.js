f86f0dcf4b33c7e87d3b10e8281aad537071cb57
Christian Brassat <elephantastic@trashbukk.it>
2019-03-19 09:16:16 +0000

    Update 'js/codeview.js'

--- js/codeview.js
+++ js/codeview.js
@@ -1,18 +1,18 @@
 $(function() {
-    $('.codeview__code').each(function() {
-	let $tabs = $(this).find('.tabs a');
-	let $tabcontents = $(this).find('.tab-content > div');
+	$('.codeview__code').each(function() {
+		let $tabs = $(this).find('.tabs a');
+		let $tabcontents = $(this).find('.tab-content > div');
 
-	$tabs.on('click', function(e) {
-	    let $target = $($(this).attr('href'));
+		$tabs.on('click', function(e) {
+			let $target = $($(this).attr('href'));
 
-	    $tabs.parent('li').removeClass('is-active');
-	    $tabcontents.removeClass('is-active');
+			$tabs.parent('li').removeClass('is-active');
+			$tabcontents.removeClass('is-active');
 
-	    $(this).parent('li').addClass('is-active');
-	    $target.addClass('is-active');
+			$(this).parent('li').addClass('is-active');
+			$target.addClass('is-active');
 
-	    e.preventDefault();
+			e.preventDefault();
+		});
 	});
-    });
 });
