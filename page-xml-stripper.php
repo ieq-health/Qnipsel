daae1f36ac040855fe1d0e6508621b17b8cfd95a
Christian Brassat <elephantastic@trashbukk.it>
2019-03-20 13:34:34 +0000

    Update 'page-xml-stripper.php'

--- page-xml-stripper.php
+++ page-xml-stripper.php
@@ -8,6 +8,88 @@
 					<h1 class="title"><?php the_title(); ?></h1>
 				</div>
 			</div>
+	
+			<?php
+				$sections = carbon_get_the_post_meta('crb_sections');
+				foreach ($sections as $section) {
+					switch ($section['_type']) {
+
+						case 'columns': ?>
+							<div class="container">
+								<div class="columns">
+									<?php foreach ($section['crb_columns'] as $column): ?>
+										<div class="column">
+											<?php if ($column['_type'] == 'text'): ?>
+												<?= templateq_text_block($column['text']); ?>
+											<?php endif; ?>
+
+											<?php if ($column['_type'] == 'message'): ?>
+												<?= templateq_message_block($column['style'], $column['title'], $column['body']); ?>
+											<?php endif; ?>
+
+											<?php if ($column['_type'] == 'code'): ?>
+												<?= templateq_code_block($column['language'], $column['code']); ?>
+											<?php endif; ?>
+										</div>
+									<?php endforeach; ?>
+								</div>
+							</div>
+						<?php break;
+
+						case 'tabs': ?>
+							<div class="tabview">
+								<div class="container">
+									<div class="tabs">
+										<ul>
+											<?php foreach ($section['crb_tabs'] as $tab): ?>
+												<li><a href="#<?= sanitize_title($tab['title']); ?>"><?= $tab['title']; ?></a></li>
+											<?php endforeach; ?>
+										</ul>
+									</div>
+
+									<div class="tab-content">
+										<?php foreach ($section['crb_tabs'] as $tab): ?>
+											<div id="<?= sanitize_title($tab['title']); ?>">
+												<?php if ($tab['_type'] == 'text'): ?>
+													<?= templateq_text_block($tab['text']); ?>
+												<?php endif; ?>
+
+												<?php if ($tab['_type'] == 'code'): ?>
+													<?= templateq_code_block($tab['language'], $tab['code']); ?>
+												<?php endif; ?>
+											</div>
+										<?php endforeach; ?>
+									</div>
+								</div>
+							</div>
+						<?php break;
+
+						case 'text': ?>
+							<div class="container">
+								<?= templateq_text_block($section['text']); ?>
+							</div>
+						<?php break;
+
+						case 'message': ?>
+							<div class="container">
+								<?= templateq_message_block($section['style'], $section['title'], $section['body']); ?>
+							</div>
+						<?php break;
+
+						case 'code': ?>
+							<div class="container">
+								<?= templateq_code_block($section['language'], $section['code']); ?>
+							</div>
+						<? break;
+
+						case 'codepen':
+							echo templateq_codeview_block($section['title'], $section['html'], $section['css'], $section['css_libs'], $section['js'], $section['js_libs']);
+						break;
+
+						case 'hr': echo '<hr>';
+					}
+				}
+			?>
 
 			<div class="container-fluid xml-stripper">
 				<div class="columns">
