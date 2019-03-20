d4bcc387ca6b500bfad51f8ae7e9e366cfcf555a
Christian Brassat <elephantastic@trashbukk.it>
2019-03-20 07:23:47 +0000

    Update 'content_blocks/codeview.php'

--- content_blocks/codeview.php
+++ content_blocks/codeview.php
@@ -1,6 +1,6 @@
 <?php
 
-function templateq_codeview_block($title, $html, $css, $js)
+function templateq_codeview_block($title, $html, $css, $css_libs, $js, $js_libs)
 { ?>
 	<div class="codeview__code">
 
@@ -25,7 +25,7 @@ function templateq_codeview_block($title, $html, $css, $js)
 			<div class="level-right">
 			<div class="level-item">
 				<div class="field is-grouped">
-				<?php if (in_array('bootstrap', $section['css_libs'])): ?>
+				<?php if (in_array('bootstrap', $css_libs)): ?>
 					<div class="control">
 						<div class="tags has-addons">
 							<div class="tag is-dark">CSS</div>
@@ -34,7 +34,7 @@ function templateq_codeview_block($title, $html, $css, $js)
 					</div>
 				<?php endif; ?>
 
-				<?php if (in_array('slick', $section['css_libs'])): ?>
+				<?php if (in_array('slick', $css_libs)): ?>
 					<div class="control">
 						<div class="tags has-addons">
 							<div class="tag is-dark">CSS</div>
@@ -43,7 +43,7 @@ function templateq_codeview_block($title, $html, $css, $js)
 					</div>
 				<?php endif; ?>
 
-				<?php if (in_array('bootstrap', $section['js_libs'])): ?>
+				<?php if (in_array('bootstrap', $js_libs)): ?>
 					<div class="control">
 						<div class="tags has-addons">
 							<div class="tag is-dark">JS</div>
@@ -52,7 +52,7 @@ function templateq_codeview_block($title, $html, $css, $js)
 					</div>
 				<?php endif; ?>
 
-				<?php if (in_array('jquery', $section['js_libs'])): ?>
+				<?php if (in_array('jquery', $js_libs)): ?>
 					<div class="control">
 						<div class="tags has-addons">
 							<div class="tag is-dark">JS</div>
@@ -61,7 +61,7 @@ function templateq_codeview_block($title, $html, $css, $js)
 					</div>
 				<?php endif; ?>
 
-				<?php if (in_array('slick', $section['js_libs'])): ?>
+				<?php if (in_array('slick', $js_libs)): ?>
 					<div class="control">
 						<div class="tags has-addons">
 							<div class="tag is-dark">JS</div>
@@ -100,11 +100,11 @@ function templateq_codeview_block($title, $html, $css, $js)
 
 		iframeDocument.open();
 
-		<?php if (in_array('bootstrap', $section['css_libs'])): ?>
+		<?php if (in_array('bootstrap', $css_libs)): ?>
 			iframeDocument.write('<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">');
 		<?php endif; ?>
 
-		<?php if (in_array('slick', $section['css_libs'])): ?>
+		<?php if (in_array('slick', $css_libs)): ?>
 			iframeDocument.write('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">');
 			iframeDocument.write('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css">');
 		<?php endif; ?>
@@ -113,16 +113,16 @@ function templateq_codeview_block($title, $html, $css, $js)
 		iframeDocument.write(`<style><?= $css ?></style>`);
 		iframeDocument.write(`<body><?= $html ?></body>`);
 
-		<?php if (in_array('jquery', $section['js_libs'])): ?>
+		<?php if (in_array('jquery', $js_libs)): ?>
 			iframeDocument.write('<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" crossorigin="anonymous"><\/script>');
 		<?php endif; ?>
 
-		<?php if (in_array('bootstrap', $section['js_libs'])): ?>
+		<?php if (in_array('bootstrap', $js_libs)): ?>
 			iframeDocument.write('<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" crossorigin="anonymous"><\/script>');
 			iframeDocument.write('<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" crossorigin="anonymous"><\/script>');
 		<?php endif; ?>
 
-		<?php if (in_array('slick', $section['js_libs'])): ?>
+		<?php if (in_array('slick', $js_libs)): ?>
 			iframeDocument.write('<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" crossorigin="anonymous"><\/script>');
 			iframeDocument.write('<script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js" crossorigin="anonymous"><\/script>');
 		<?php endif; ?>
