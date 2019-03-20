894d9144ff1091d9ca990b5eda251e36ef99d294
Christian Brassat <elephantastic@trashbukk.it>
2019-03-20 10:45:28 +0000

    Update 'content.php'

--- content.php
+++ content.php
@@ -89,6 +89,9 @@ foreach ($sections as $section) {
 		
 		case 'codepen':
 			echo templateq_codeview_block($section['title'], $section['html'], $section['css'], $section['css_libs'], $section['js'], $section['js_libs']);
+			break;
+
+		case 'hr': echo '<hr>';
 	}
 }
 ?>
