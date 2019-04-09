0996fa913719af49d9bdab83b906264e04da786c
Christian Brassat <elephantastic@trashbukk.it>
2019-04-09 14:15:52 +0000

    Update 'index.php'

--- index.php
+++ index.php
@@ -5,4 +5,21 @@
 		the_post();
 		get_template_part('content', get_post_format());
 	}
-} ?>
+} else { ?>
+<div id="site-container">
+	<main>
+		<div class="container">
+			<div class="content">
+				<h1 class="title">Letzte Updates</h1>
+			</div>
+		</div>
+		<div class="container">
+			<div class="columns">
+				<div class="column">
+					<?= templateq_recent_updates(); ?>
+				</div>
+			</div>
+		</div>
+	</main>
+</div>
+<?php } ?>
