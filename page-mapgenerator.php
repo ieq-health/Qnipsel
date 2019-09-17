150ed340f7708aad29a7b1062a9b5a5f8b60cd7b
Christian Brassat <christian.brassat@gmail.com>
2019-09-17 13:55:29 +0200

    Update page-mapgenerator.php

---
+++ page-mapgenerator.php
@@ -0,0 +1,182 @@
+<?php get_header(); ?>
+
+<div id="site-container">
+	<main>
+		<?php if (have_posts()): ?>
+			<?php while (have_posts()): the_post(); ?>
+				<div class="container">
+					<div class="content">
+						<h1 class="title"><?php the_title(); ?></h1>
+					</div>
+				</div>
+
+				<div class="container-fluid xml-stripper">
+					<div class="columns">
+						<div class="column">
+							<p class="has-text-grey is-uppercase is-size-7">Input</p>
+
+							<div class="columns">
+								<div class="column">
+									<div class="field">
+										<label class="label">Location</label>
+
+										<div class="control">
+											<input name="lat" type="text" class="input" placeholder="Latitude">
+										</div>
+
+										<div class="control">
+											<input name="lng" type="text" class="input" placeholder="Longitude">
+										</div>
+
+										<div class="control">
+											<input name="zoom" type="text" class="input" placeholder="Zoom">
+										</div>
+									</div>
+								</div>
+
+								<div class="column">
+									<div class="field">
+										<label class="label">Offset LG</label>
+
+										<div class="control">
+											<input name="lat_offset" value="0" type="text" class="input" placeholder="Latitude">
+										</div>
+
+										<div class="control">
+											<input name="lng_offset" value="0" type="text" class="input" placeholder="Longitude">
+										</div>
+
+										<div class="control">
+											<input name="zoom_offset" type="text" class="input" placeholder="Zoom">
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<!-- Styles -->
+
+							<div class="field">
+								<label class="label">Styles</label>
+								<div class="control">
+									<textarea name="styles" class="textarea has-border"></textarea>
+								</div>
+							</div>
+
+							<!-- Marker -->
+
+							<div class="column">
+								<div class="field">
+									<label class="label">Marker</label>
+									<div class="control"><input type="text" name="marker"></div>
+									<div class="control"><input type="text" name="marker_width"></div>
+									<div class="control"><input type="text" name="marker_height"></div>
+									<div class="control"><input type="text" name="title"></div>
+									<div class="control"><input type="text" name="url"></div>
+								</div>
+							</div>
+
+						</div>
+
+						<div class="column has-background-light">
+							<p class="has-text-grey is-uppercase is-size-7">Output</p>
+							<textarea class="textarea is-family-code" name="" id="output" cols="30" rows="10" readonly></textarea>
+						</div>
+					</div>
+				</div>
+			<?php endwhile; ?>
+		<?php endif; ?>
+	</main>
+	<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+<script>
+	$(function() {
+
+		$('input, select, .field textarea').on('input propertychange', function() {
+			let lat = $('input[name="lat"]').val()
+			let lng = $('input[name="lng"]').val()
+			let zoom = $('input[name="zoom"]').val()
+
+			let lat_offset = lat + $('input[name="lat_offset"]').val();
+			let lng_offset = lng + $('input[name="lng_offset"]').val();
+			let zoom_offset = zoom + $('input[name="zoom_offset"]').val();
+
+			let styles = $('textarea[name="styles"]').val();
+
+			let marker = $('input[name="marker"]').val();
+			let marker_width = $('input[name="marker_width"]').val();
+			let marker_height = $('input[name="marker_height"]').val();
+
+			let title = $('input[name="title"]').val();
+			let url = $('input[name="url"]').val();
+
+			$('#output').val(`
+
+const mq = window.matchMedia('screen and (min-width: 1200px)');
+
+const mapLocation = {
+	lat: ${lat},
+	lng: ${lng},
+	zoom: ${zoom},
+
+	get offset() {
+		return {
+			lat: ${lat_offset},
+			lng: ${lng_offset},
+			zoom: ${zoom_offset}
+		}
+	}
+};
+
+function initMap() {
+	const map = new google.maps.Map(document.getElementById('map'), {
+		center: mq.matches ? mapLocation.offset : mapLocation,
+		zoom: mq.matches ? mapLocation.offset.zoom : mapLocation.zoom,
+		disableDefaultUI: true,
+		styles: [ ${styles} ]
+	});
+
+	const image = {
+		url: '${marker}',
+		size: new google.maps.Size(${marker_width}, ${marker_height}),
+		anchor: new google.maps.Point(${marker_width / 2}, ${marker_height})
+	};
+
+	const marker = new google.maps.Marker({
+		position: mapLocation,
+		map: map,
+		title: '${title}',
+		icon: image,
+		url: '${url}'
+	});
+
+	google.maps.event.addListener(marker, "click", function() {
+		window.open(this.url, "_blank");
+	});
+
+	window.addEventListener('resize', debounce(function() {
+		map.setCenter(mq.matches ? mapLocation.offset : mapLocation)
+		map.setZoom(mq.matches ? mapLocation.offset.zoom : mapLocation.zoom)
+	}, 200, false));
+}
+
+function debounce(func, wait, immediate) {
+	let timeout;
+	return function() {
+		const context = this;
+		const args = arguments;
+		const later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		const callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	}
+}
+
+				`);
+		});
+	});
+</script>
+
+<?php get_footer(); ?>
