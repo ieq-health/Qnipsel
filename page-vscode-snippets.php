7b35a97381d253f6684ef6341954351c9e176d19
Christian Brassat <christian@brassat.eu>
2019-10-28 12:10:13 +0100

    this doesn't need a submenu

--- page-vscode-snippets.php
+++ page-vscode-snippets.php
@@ -108,17 +108,6 @@ parseDescription(carbon_get_the_post_meta('description')) . '
 			<?php endwhile; ?>
 		<?php endif; ?>
 	</main>
-
-	<aside class="menu" id="dennisMenu">
-		<nav>
-			<ul class="menu-list">
-				<?php wp_list_pages(array(
-					'title_li' => null,
-					'walker' => new Templateq_Walker()
-				)); ?>
-			</ul>
-		</nav>
-	<aside>
 </div>
 
 <footer class="footer has-text-centered">
