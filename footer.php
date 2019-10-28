4c687389ac56e9ff912ec5865c0a18b444e9a8b2
Christian Brassat <christian@brassat.eu>
2019-10-28 16:23:27 +0100

    remove stray closing div

--- footer.php
+++ footer.php
@@ -1,5 +1,4 @@
-            <?php get_template_part('partials/menu', 'side'); ?>
-        </div>
+        <?php get_template_part('partials/menu', 'side'); ?>
 
         <footer class="footer has-text-centered">
             <?php the_author(); ?> | <?= the_date(); ?> &mdash; <?php the_modified_author(); ?> | <?= the_modified_date(); ?>
