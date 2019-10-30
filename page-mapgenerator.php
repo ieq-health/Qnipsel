ca0905d13c89cdc3664d7ebdb998b69620a7c688
Christian Brassat <christian@brassat.eu>
2019-10-30 07:49:16 +0100

    remove title on map generator

--- page-mapgenerator.php
+++ page-mapgenerator.php
@@ -4,8 +4,6 @@
 	<main>
 		<?php if (have_posts()): ?>
 			<?php while (have_posts()): the_post(); ?>
-				<?php get_template_part('partials/content', 'title'); ?>
-
 				<div class="container-fluid xml-stripper">
 					<div class="columns">
 						<div class="column">
