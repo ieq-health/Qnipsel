42bdc32be07ac27ec035e0688f9d3ffb60d1bf57
Christian Brassat <christian@brassat.eu>
2019-11-07 16:05:26 +0100

    split menu walkers

--- functions.php
+++ functions.php
@@ -6,6 +6,11 @@
 
 require_once( __DIR__ . '/fractals/bulma/forms.php');
 
+/** Walkers
+ * Declare our own menu walkers
+ */
+require_once( __DIR__ . '/include/walkers.php');
+
 /** Custom Fields
  * Set up the Custom Fields plugin and define some fields.
  */
@@ -317,49 +322,3 @@ function templateq_snippet_post_type()
 	));
 }
 add_action('init', 'templateq_snippet_post_type');
-
-/** Custom Menu Walker */
-
-class Templateq_Split_Nav_Sidenav_Walker extends Walker_Page
-{
-	public function start_lvl(&$output, $depth=0, $args=array())
-	{
-		$output .= '<ul class="menu-list">';
-	}
-
-	public function start_el(&$output, $page, $depth=0, $args=array(), $id=0)
-	{
-		$class = ($page->ID == $id) ? 'is-active' : ''; 
-		$link = '<a href="' . get_permalink($page->ID) . '" class="' . $class . '">' . $page->post_title . '</a>'; 
-		$output .= '<li>' . $link;
-	}
-
-	public function end_el(&$output, $page, $depth=0, $args=array())
-	{
-		$output .= '</li>';
-	}
-}
-
-/** Split Nav Walker */
-class Templateq_Split_Nav_Topnav_Walker extends Walker_Page
-{
-	public function start_lvl(&$output, $depth=0, $args=array())
-	{
-	}
-
-	public function start_el(&$output, $page, $depth=0, $args=array(), $id=0)
-	{
-		if ($depth == 0) {
-			$class = ($page->ID == $id) ? 'is-active' : '';
-			$output .= '<a class="navbar-item ' . $class . '" href="' . get_permalink($page->ID) . '">' . $page->post_title . '</a>';
-		}
-	}
-
-	public function end_el(&$output, $page, $depth=0, $args=array(), $id=0)
-	{
-	}
-
-	public function end_lvl(&$output, $depth=0, $args=array())
-	{
-	}
-}
