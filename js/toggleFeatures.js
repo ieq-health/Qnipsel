de703a2f3f552055d0d2fc16d1eb32b3ddf22a06
Christian Brassat <christian@brassat.eu>
2019-11-21 16:01:08 +0100

    enable dark mode based on prefers-color-scheme

--- js/toggleFeatures.js
+++ js/toggleFeatures.js
@@ -15,3 +15,11 @@ const toggleFeature = (feature, enabled, setCookie = false) => {
 		toggleFeature(feature, $(event.target).is(':checked'), true);
 	});
 });
+
+/**
+ * Turn on dark mode if 'prefers-color-scheme: dark' matches and cookie is not set
+ */
+
+if ( typeof Cookies.get('darkMode') === 'undefined' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+	toggleFeature('darkMode', true, true);
+}
