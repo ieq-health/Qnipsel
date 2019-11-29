74b7454de239a158668658af1f9619b3be4979c5
Christian Brassat <christian@brassat.eu>
2019-11-29 08:59:10 +0100

    bump version

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 /** Set version
  * Used for cachebusting JS/CSS
  */
-$GLOBALS['qnipsel_version'] = '0.8.0';
+$GLOBALS['qnipsel_version'] = '0.8.2';
 
 /** Custom Fields
  * Set up the Custom Fields plugin and define some fields.
