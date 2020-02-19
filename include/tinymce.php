283e5e0e9425e047bcb27466dea9199d690aa7cd
Christian Brassat <christian@brassat.eu>
2020-02-19 11:33:50 +0100

    add inline code button to mce_buttons

--- include/tinymce.php
+++ include/tinymce.php
@@ -1,7 +1,11 @@
 <?php
 
-// Add code button
-add_filter('mce_buttons_2', function($buttons) {
-  $buttons[] = 'code';
+add_filter('mce_buttons', function($buttons) {
+  array_splice($buttons, 6, 0, array('templateq_code_button', 'separator'));
   return $buttons;
+});
+
+add_filter('mce_external_plugins', function($plugin_array) {
+  $plugin_array['templateq_code_button'] = get_template_directory_uri() . '/backend/tinymce-code-button.js';
+  return $plugin_array;
 });
