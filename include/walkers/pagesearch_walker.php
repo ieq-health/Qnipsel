edc51c57d4baab98465f1f64fac4699aeb771e42
Christian Brassat <christian@brassat.eu>
2020-04-15 14:00:44 +0200

    display parent page title in search

--- include/walkers/pagesearch_walker.php
+++ include/walkers/pagesearch_walker.php
@@ -8,6 +8,10 @@ class Templateq_Pagesearch_Walker extends Walker_Page
 
 	public function start_el(&$output, $page, $depth = 0, $args = array(), $id = 0)
 	{
+		if ( 0 != $page->post_parent ) {
+			$output .= '<small>' . get_the_title($page->post_parent) . '</small>';
+		}
+
 		$output .= '<li><a href="' . get_permalink($page->ID) . '">' . $page->post_title . '</a></li>';
 	}
 
