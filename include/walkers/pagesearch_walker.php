48e9013b4675698012b9814aa17c3c248271d2d6
Christian Brassat <christian@brassat.eu>
2020-04-15 14:05:49 +0200

    actually display the parent inside the link

--- include/walkers/pagesearch_walker.php
+++ include/walkers/pagesearch_walker.php
@@ -8,11 +8,13 @@ class Templateq_Pagesearch_Walker extends Walker_Page
 
 	public function start_el(&$output, $page, $depth = 0, $args = array(), $id = 0)
 	{
+		$output .= '<li><a href="' . get_permalink($page->ID) . '">';
+
 		if (0 != $page->post_parent) {
 			$output .= '<small>' . get_the_title($page->post_parent) . '</small>';
 		}
 
-		$output .= '<li><a href="' . get_permalink($page->ID) . '">' . $page->post_title . '</a></li>';
+		$output .= $page->post_title . '</a></li>';
 	}
 
 	public function end_el(&$output, $page, $depth = 0, $args = array())
