b1410ee7be6c615ebd23759465c57bb02cf83b2e
Dennis Lennemann <61309536+weirdodev@users.noreply.github.com>
2020-07-27 14:14:15 +0200

    Update page-faqgenerator.php

--- page-faqgenerator.php
+++ page-faqgenerator.php
@@ -19,10 +19,10 @@
                                         <div class="faq__item columns">
                                             <div class="faq__item__content column">
                                                 <div class="control is-expanded">
-                                                    <input type="text" class="input faq_question" name="frage" placeholder="Frage">
+                                                    <input type="text" class="input faq_question" name="question" placeholder="Frage">
                                                 </div>
                                                 <div class="control is-expanded">
-                                                    <input type="text" class="input" name="antwort" placeholder="Antwort">
+                                                    <input type="text" class="input" name="answer" placeholder="Antwort">
                                                 </div>
                                             </div>
                                             <button class="faq__item__delete"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg></button>
