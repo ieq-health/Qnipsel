22482dc317ffa62195e649f8d75605de2933ec03
Dennis Lennemann <61309536+weirdodev@users.noreply.github.com>
2020-07-27 14:44:40 +0200

    Update page-faqgenerator.php

--- page-faqgenerator.php
+++ page-faqgenerator.php
@@ -28,9 +28,10 @@
                                             <button class="faq__item__delete button is-danger is-outlined is-small"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg></button>
                                         </div>
                                     </div>
-                
+									
+									<button class="faq-add button is-outlined">weitere hinzufügen</button>
                                 </div>
-                                <button class="faq-add button is-outlined">weitere hinzufügen</button>
+                        
                                 <button class="faq-generate button is-primary">Give me Dat Code</button>
                             </div>
                         </div>
