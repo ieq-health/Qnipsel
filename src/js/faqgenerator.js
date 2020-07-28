ca69d59251fc12060e181c13be216b9692025f9f
Dennis Lennemann <61309536+weirdodev@users.noreply.github.com>
2020-07-28 14:24:48 +0200

    prettified output of json

--- src/js/faqgenerator.js
+++ src/js/faqgenerator.js
@@ -13,11 +13,11 @@ $('.faq-add').click(function() {
                     '<input type="text" class="input" name="answer_' + faqcount + '" placeholder="Antwort">',
                 '</div>',
             '</div>',
-            '<span class="faq__item__delete"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z"/></svg></span>',
+            '<button class="faq__item__delete"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z"/></svg></button>',
         '</div>'
         ].join("\n");
 
-    $('.faq').append(faq); // add another faq
+    $('.field').append(faq); // add another faq
 });
 
 
@@ -54,9 +54,5 @@ $('.faq-generate').click(function(){  // generate json
        
     });            
 
-    $('.faq-generator__output').val(`
-        <script type="application/ld+json">
-            ${JSON.stringify(jsonFAQ)}
-        <\/script>`);
-
+    $('.faq-generator__output').val(`<script type="application/ld+json">${JSON.stringify(jsonFAQ,null,'\t')}<\/script>`);
 });
