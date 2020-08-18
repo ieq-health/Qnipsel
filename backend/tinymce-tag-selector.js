089958bbd122f868e01e4ad257b3166156aa8afd
Christian Brassat <christian@brassat.eu>
2020-08-18 08:11:31 +0200

    add tag button to tinyMCE

---
+++ backend/tinymce-tag-selector.js
@@ -0,0 +1,39 @@
+(function () {
+	tinymce.PluginManager.add("templateq_tag_selector", function (editor, url) {
+		function makeTag(color) {
+			editor.focus();
+			editor.selection.setContent('<span class="tag is-' + color + '">' + editor.selection.getContent() + "</span>");
+		}
+
+		editor.addButton("templateq_tag_selector", {
+			title: "Tag",
+			icon: "wp_page",
+			type: "menubutton",
+			menu: [{
+        text: "Primary",
+        value: "Primary",
+        onclick: function () { makeTag("primary") },
+      },
+      {
+        text: "Success",
+        value: "Success",
+        onclick: function () { makeTag("success") },
+      },
+      {
+        text: "Warning",
+        value: "Warning",
+        onclick: function () { makeTag("warning") },
+      },
+      {
+        text: "Danger",
+        value: "Danger",
+        onclick: function () { makeTag("danger") },
+      },
+      {
+        text: "Info",
+        value: "Info",
+        onclick: function () { makeTag("info") },
+      }],
+		});
+	});
+})();
--- include/tinymce.php
+++ include/tinymce.php
@@ -1,11 +1,12 @@
 <?php
 
 add_filter('mce_buttons', function ($buttons) {
-	array_splice($buttons, 6, 0, array('templateq_code_button', 'separator'));
+	array_splice($buttons, 6, 0, array('templateq_code_button', 'separator', 'templateq_tag_selector'));
 	return $buttons;
 });
 
 add_filter('mce_external_plugins', function ($plugin_array) {
 	$plugin_array['templateq_code_button'] = get_template_directory_uri() . '/backend/tinymce-code-button.js';
+	$plugin_array['templateq_tag_selector'] = get_template_directory_uri() . '/backend/tinymce-tag-selector.js';
 	return $plugin_array;
 });
