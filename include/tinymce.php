d6711db20fbebfcfd41516d49dd75132572bde5f
Christian Brassat <christian@brassat.eu>
2020-08-18 08:32:05 +0200

    add tag styles to tinyMCE

--- include/tinymce.php
+++ include/tinymce.php
@@ -6,7 +6,12 @@ add_filter('mce_buttons', function ($buttons) {
 });
 
 add_filter('mce_external_plugins', function ($plugin_array) {
+	// Add button to create inline code snippets 
 	$plugin_array['templateq_code_button'] = get_template_directory_uri() . '/backend/tinymce-code-button.js';
+
+	// Add selector to create tags
 	$plugin_array['templateq_tag_selector'] = get_template_directory_uri() . '/backend/tinymce-tag-selector.js';
+	add_editor_style('/backend/tinymce-tag-styles.css');
+
 	return $plugin_array;
 });
