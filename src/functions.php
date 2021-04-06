9cac370e6634a23a2e2d0e0a70c165794f7953de
Christian Brassat <christian.brassat@gmail.com>
2021-04-06 13:25:59 +0200

    update changelog; bump version

--- src/functions.php
+++ src/functions.php
@@ -3,7 +3,7 @@
 /** Set version
  * Used for cachebusting JS/CSS
  */
-$GLOBALS['qnipsel_version'] = '0.18.3';
+$GLOBALS['qnipsel_version'] = '0.18.4';
 
 /** Custom Fields
  * Set up the Custom Fields plugin and define some fields.
